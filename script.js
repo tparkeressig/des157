1f1edb0da2986e3252fdeaebfc0c0d69d8ee1ffe
Tara Parker-Essig <tparkeressig@ucdavis.edu>
2017-01-19 13:32:11 -0800

    Link fixes, script update

--- script.js
+++ script.js
@@ -5,22 +5,27 @@
 console.log("The js is being read.");
 
 function setup() {
-  var canvas = createCanvas(800, 250);
-  // Move the canvas so it's inside <div id="sketch-holder">.
-  canvas.parent('p5');
-  background(0);
+    var canvas = createCanvas(800, 250);
+    // Move the canvas so it's inside <div id="sketch-holder">.
+    canvas.parent('p5');
+    background(255);
+
+    noStroke();
 }
 
 
 function draw() {
-  if(mouseIsPressed){
-    fill (127);
-  }else{
-    fill(255);
-  }
-  if (keyIsPressed){
-  rect(mouseX,mouseY,100,100);
-  }else{
-  rect(mouseX,mouseY,50,50);
-  }
+    var ran = random(0, 225);
+
+    if (mouseIsPressed) {
+        fill(ran);
+    } else {
+        fill(0);
+    }
+    if (keyIsPressed) {
+        rect(mouseX, mouseY, 100, 100);
+    } else {
+        triangle(mouseX + 30, mouseY + 75, mouseX + 58, mouseY + 20, mouseX + 86, mouseY + 75);
+    }
+
 }
