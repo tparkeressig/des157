19aa426683e781867ddcc750fe8e200804d505ab
Tara Parker-Essig <tparkeressig@ucdavis.edu>
2017-01-26 23:08:13 -0800

    All five text fields return properly! YAY!
    Success with the bare minimum of the assignment!

--- studio2/script.js
+++ studio2/script.js
@@ -5,29 +5,35 @@ console.log("reading");
 document.addEventListener("DOMContentLoaded", function(event) {
     console.log("DOM fully loaded and parsed");
 
-    // all other js below here
-
-    //capture the submit event
+    //First, capture the submit event
     document.theForm.onsubmit = processForm;
 
-    //define process function
+    //Next, define process function (now we'll be inside the function!!!)
     function processForm() {
 
-      //store user name in a variable
-      var userName = document.theForm.userName.value;
-      //alert('Hi ' + userName);
+      //Third, store user name in a variable called userNameJS
+      var userNameJS = document.theForm.userName.value;
 
-      //store userColor in a variable called userColor
-      var userProfesh = document.theForm.userProfesh1.value;
+      //Fourth, store "something yummy" in a variable called userYumJS
+      var userYumJS = document.theForm.userYum.value;
 
-      //comment out the alert message above
+      //Fifth, store the color the user imput in a variable called userColorJS
+      var userColorJS = document.theForm.userColor.value;
 
-      //apply the method of innerHTML to concantenate a message with userName and userColor
-      myMsg.innerHTML="What's up, " + userName + "? I hear you're an expert " + userProfesh + " designer !";
+      //Fifth, store the animal the user imput in a variable called userAnimalJS
+      var userAnimalJS = document.theForm.userAnimal.value;
 
-      //prevent page from reloading
+      //Fifth, store the "good thing" the user imput in a variable called goodThingJS
+      var goodThingJS = document.theForm.goodThing.value;
+
+      //Finally, apply the method of innerHTML to concantenate a message with the variables you've stored earlier.
+      msg1.innerHTML="What's up, " + userNameJS + "? Let's bake " + userYumJS + " cookies!";
+      msg2.innerHTML="Let's cut them out in the shape of a " + userAnimalJS + " and put " + userColorJS + " frosting on them!";
+      msg3.innerHTML="Wow, these look great! You must have put so much " + goodThingJS + " into making them!";
+
+      //Also, prevent page from reloading
       return false;
 
     }
-
+//Now we're back outside the function!
 });
