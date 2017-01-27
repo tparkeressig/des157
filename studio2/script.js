47bb1331491205e59b28b9202241561687089efb
Tara Parker-Essig <tmape@scc2102-3.ucdavis.edu>
2017-01-26 16:30:38 -0800

    Replaced all my form with what worked on code pen... still doesn't work. ):

--- studio2/script.js
+++ studio2/script.js
@@ -8,22 +8,22 @@ document.addEventListener("DOMContentLoaded", function(event) {
     // all other js below here
 
     //capture the submit event
-    document.f.onsubmit = processForm;
+    document.theForm.onsubmit = processForm;
 
     //define process function
     function processForm() {
 
       //store user name in a variable
-      var userName = document.f.userName.value;
+      var userName = document.theForm.userName.value;
       //alert('Hi ' + userName);
 
       //store userColor in a variable called userColor
-      var userColor = document.f.userColor.value;
+      var userColor = document.theForm.userColor.value;
 
       //comment out the alert message above
 
       //apply the method of innerHTML to concantenate a message with userName and userColor
-      myMsg.innerHTML="What's up, " + userName + ", I heard you like the color " + userColor + "!";
+      myMsg.innerHTML="What's up, " + userName + "? I hear you're an expert " + userProfesh + " designer !";
 
       //prevent page from reloading
       return false;
