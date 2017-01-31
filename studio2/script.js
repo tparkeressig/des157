d4e9424db3562abf6cd9a1435fdffa4df029d8f5
T P-E <lgbtresourcecenter@campus-029-165.ucdavis.edu>
2017-01-31 13:08:52 -0800

    added js element that always returns cookies, also edited some copy and comments

--- studio2/script.js
+++ studio2/script.js
@@ -11,25 +11,28 @@ document.addEventListener("DOMContentLoaded", function(event) {
     //Next, define process function(s) (we'll now be inside the function!!!)
     function processForm() {
 
-      //Third, store user name in a variable called userNameJS
+      //Third, store player's name in a variable called userNameJS
       var userNameJS = document.theForm.userName.value;
 
       //Fourth, store "something yummy" in a variable called userYumJS
       var userYumJS = document.theForm.userYum.value;
 
-      //Fifth, store the color the user imput in a variable called userColorJS
+      //Fifth, store the favorite color the user imput in a variable called userColorJS
       var userColorJS = document.theForm.userColor.value;
 
-      //Fifth, store the animal the user imput in a variable called userAnimalJS
+      //Fifth, store the chosen animal the user imput in a variable called userAnimalJS
       var userAnimalJS = document.theForm.userAnimal.value;
 
       //Fifth, store the "good thing" the user imput in a variable called goodThingJS
       var goodThingJS = document.theForm.goodThing.value;
 
+      //Sixth, store the dropdown selection response in a variable called foodTypeJS
+      var foodTypeJS = "cookies"
+
       //Finally, apply the method of innerHTML to concantenate a message with the variables you've stored earlier.
-      msg1.innerHTML="What's up, " + userNameJS + "? Let's bake " + userYumJS + " cookies!";
+      msg1.innerHTML="What's up, " + userNameJS + "? Let's bake " + userYumJS + " " + foodTypeJS + "!";
       msg2.innerHTML="Let's cut them out in the shape of a " + userAnimalJS + " and put " + userColorJS + " frosting on them!";
-      msg3.innerHTML="Wow, these look great! You must have put so much " + goodThingJS + " into making them!";
+      msg3.innerHTML="Wow, your " + foodTypeJS + " look great! You must have put so much " + goodThingJS + " into making them!";
 
       //Also, prevent page from reloading
       return false;
