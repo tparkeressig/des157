0b70cebb06b4119cf1578941dc741e2ae3060059
Tara Parker-Essig <tmape@scc2102-13.ucdavis.edu>
2017-02-02 15:31:58 -0800

    Set up basic layout with relatively positioned h1 and prepped image

---
+++ studio3/script.js
@@ -0,0 +1,10 @@
+// This is Tara's Studio 3 DES 157 JavaScript Document
+
+console.log("Reading JS");
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    console.log("DOM fully loaded and parsed");
+
+    // all other js here
+
+});
