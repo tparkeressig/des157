3aace49ae65002c5d6db979d38e5c76cef67939f
Tara Parker-Essig <tparkeressig@ucdavis.edu>
2017-02-06 03:10:05 -0800

    Added popup div that displays on clicking the button

--- studio3/script.js
+++ studio3/script.js
@@ -7,7 +7,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     // all other js goes here inside the addEventListener and function brackets
 
+    // let's gather all the html elements we will be influencing here:
     var basicButtonJS = document.getElementById('basicButton');
+    var popup0JS = document.getElementById('popup0')
 
     // on mouseover do these things to the #basicButton:
     basicButtonJS.addEventListener('mouseover', function(){
@@ -16,8 +18,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
     })
     // on mouseout do these things to the #basicButton:
     basicButtonJS.addEventListener('mouseout', function(){
-    basicButton.style.backgroundColor="lightblue";
-    basicButton.innerHTML = "This should change color when hovered over!";
+      basicButton.style.backgroundColor="lightblue";
+      basicButton.innerHTML = "This should change color when hovered over!";
+    })
+    // on click, do these things to the #basicButton:
+    basicButtonJS.addEventListener('click', function(){
+      popup0.style.display="block";
     })
 
 });
