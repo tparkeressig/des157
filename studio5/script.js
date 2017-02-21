bdcf73e21b9fee76dbd42f49d2b5283c2eefd423
Tara Parker-Essig <tparkeressig@ucdavis.edu>
2017-02-20 16:30:07 -0800

    Added linkage from script to html to css etc

---
+++ studio5/script.js
@@ -0,0 +1,17 @@
+// This is Tara's Studio 5 DES 157 JavaScript Document
+
+console.log("Reading JS");
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  console.log("DOM fully loaded and parsed");
+
+  // all other js goes here inside the addEventListener and function brackets
+
+
+
+
+
+  //Last but not least, prevent page from reloading
+  return false;
+
+});
