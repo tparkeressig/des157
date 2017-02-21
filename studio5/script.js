17b7a9042d9497b36df6933bd64bdd4de2ca3432
Tara Parker-Essig <tparkeressig@ucdavis.edu>
2017-02-21 00:05:42 -0800

    Crap I broke something
    But I need it hosted so ppl can trouble shoot it for me

--- studio5/script.js
+++ studio5/script.js
@@ -8,6 +8,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
   // all other js goes here inside the addEventListener and function brackets
 
   var catButton = document.getElementById("theCat"); //get the img of the cat in a variable
+  var pressedYet = false; //boolean to tell if the catButton has been pressed yet
   var purrAudio = new Audio(); //create a variable that is audio
   purrAudio.src = "./audio/purring.mp3"; //bring in the mp3 file
 
@@ -16,15 +17,22 @@ document.addEventListener("DOMContentLoaded", function(event) {
     catButton.src = "./images/sleeping-with-interaction.png"
   });
 
-  //on mouseout, cat is still sleeping but play button disappears
   catButton.addEventListener("mouseout", function() {
-    catButton.src = "./images/sleeping-no-interaction.png"
+    if (pressedYet == false) {
+      //on mouseout without click, cat is sleeping & play button disappears
+      catButton.src = "./images/sleeping-no-interaction.png"
+    }
+    if (pressedYet == true) {
+      //on mouseout after click, cat is purring & pause button disappears
+      catButton.src = "./images/purring-no-interaction.png"
+    }
   });
 
   //on click, cat begins purring and pause button appears
   catButton.addEventListener("click", function() {
     purrAudio.play();
     catButton.src = "./images/purring-with-interaction.png"
+    pressedYet = true;
   });
 
 
@@ -34,5 +42,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
   //Last but not least, prevent page from reloading
   return false;
+
+});
+eturn false;
 
 });
